51e16d2794a47109fa019722be3abdf605181f07
Sergey <sergsmile29@gmail.com>
2022-05-19 00:31:08 +0300

    Старт разработки, создание хедера и навигации

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,34 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.scss';
+
+const Header = () => {
+  return (
+      <header>
+        Добро пожаловать!
+      </header>
+  )
+}
+
+const Navigation = () => {
+  return (
+      <nav>
+        <ul>
+          <li>Моя дорожная карта</li>
+          <li>Хуки</li>
+          <li>Методичка</li>
+        </ul>
+      </nav>
+  )
+}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+      <Header/>
+        <Navigation/>
+      </main>
     </div>
   );
 }
